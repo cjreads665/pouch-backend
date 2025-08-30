b7453aab66a2c36ecaff8ff3c1f0485602f14da0
Shahid Alam <shahid.alam57@gmail.com>
2025-08-30 13:10:07 +0530

    added a root directory

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,13 @@ const PORT = process.env.PORT || 3000
 
 app.use(cors()) // Enable CORS for all routes. Change it later [IMPORTANT]
 
-app.use('/api',articleRoute)
+//http://localhost:3000/extract/article?url=https%3A%2F%2Fwww.bbc.com%2Fnews%2Fworld-60525350
+app.use('/extract',articleRoute)
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+app.use('/',(req,res)=>{
+    res.send('Welcome to Pick-Pockt API. Please use /extract/article?url=YOUR_ARTICLE_URL to fetch article content.')
 })
